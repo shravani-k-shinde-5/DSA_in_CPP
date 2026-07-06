2c7ad29995b32852a85efe299c1a7e97ef158996
shravani-k-shinde-5 <shravanishinde857@gmail.com>
2026-07-06 15:00:27 +0530

    updated duplicate code

--- 07ARRAY/duplicatesfromsortedarray.cpp
+++ 07ARRAY/duplicatesfromsortedarray.cpp
@@ -3,21 +3,14 @@
 using namespace std;
 
 void duplicates_from_sorted_array(vector<int>&arr){
-  int n=arr.size();
-  int i =0;
-  int j=0;
-
-  for(int k=0;k<=n;k++){
-    if(arr[i]==arr[j]){
-      j++;
-    }
-    else{
-      swap(arr[i],arr[j]);
+  int  i=0;
+  for(int j=0;j<arr.size();j++){
+    if(arr[i]!=arr[j]){
       i++;
+      arr[i]=arr[j];
     }
-  
   }
-}
+  return arr.resize(i+1);}
 int main(){
   int n;
   cin>>n;
