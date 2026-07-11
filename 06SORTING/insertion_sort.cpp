c979367d23290d1fa244792a3f71f19e6b7fef82
shravani-k-shinde-5 <shravanishinde857@gmail.com>
2026-07-11 14:42:36 +0530

    Update insertion_sort.cpp

--- 06SORTING/insertion_sort.cpp
+++ 06SORTING/insertion_sort.cpp
@@ -12,8 +12,17 @@ void insertion_sort(vector<int> &arr) {
       }
     }
 }
-
+void insertion_sort2(vector<int> &arr) {
     
+    for(int i =1;i<arr.size();i++){
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>temp){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
 
     
 
